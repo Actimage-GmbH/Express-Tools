a93e6c8a0126cb34cc90f31b29f12268fe53bbf3
Baptiste BELLOT <baptiste.bellot@actimage.com>
2020-10-08 07:21:20 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 
-
-const DB = require('./database');
 const ErrorHandling = require('./error-handling');
 const AccessControl = require('./access-control');
 
 module.exports = {
-    database : DB,
     errorHandler : ErrorHandling,
     accessControl : AccessControl
 };
